6055dab91850b14f6c4487f1eb968ec0a340a918
davidguerreiro <me@davidguerreiro.com>
2018-08-12 18:55:59 +0100

    add sass folder and files. Add gulpfile and add enqueue scripts file to functions.php

--- functions.php
+++ functions.php
@@ -12,4 +12,15 @@
   * Extra code file
   */
   include get_template_directory() . '/inc/extras.php';
+
+  /**
+  * Load css and js
+  *
+  * @return void
+  */
+  function g_enqueue_scripts() {
+    $version = intval( date('s') ) * rand( 1, 99999 );
+    wp_enqueue_style( '_theme-style', get_template_directory_uri() . '/css/style.css', array(), $version, 'all' );
+  }
+  add_action( 'wp_enqueue_scripts', 'g_enqueue_scripts' );
   
