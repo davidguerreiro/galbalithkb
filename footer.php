abc53586650583df5dc4c7575a84bbff597161d9
davidguerreiro <me@davidguerreiro.com>
2018-08-14 00:38:01 +0100

    add index login form HTML / CSS

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
         </main>
         <footer class="footer">
             <ul class="footer__primary-elements">
-                <li>&copy; 2011 - 2018 Galbalith</li>
+                <li>&copy; 2011 - <?php echo date('Y'); ?> Galbalith</li>
                 <li> 
                     Un juego desarrollado por <a href="https://github.com/davidguerreiro" target="_blank">David Guerreiro</a>
                 </li>
--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<p>Yes the theme works as expected</p>
+<?php
+/**
+ * Main theme file
+ * 
+ * @package galbalith
+ */
+
+
+ $cover_image = get_template_directory_uri() . '/assets/img/web_cover.png';
+ $page_id = get_the_ID();
+ $nonce = wp_create_nonce( 'login' );
+
+ 
+ get_header();
+
+ ?>
+
+    <img src="<?php echo esc_url( $cover_image ); ?>" alt="cover" class="main__cover-image">
+    <form action="" method="post" class="login-form">
+        <input type="hidden" name="page-id" value="<?php echo $page_id; ?>">
+        <input type="hidden" name="action" value="login">
+        <input type="hidden" name="nonce" value="<?php echo $nonce; ?>">
+        <div class="login-form__row">
+            <label for="user-mail" class="login-form__text-label">
+                Email :
+            </label>
+            <input type="email" name="user-email" id="user-email" class="login-form__text-input" required>
+        </div>
+        <div class="login-form__row">
+            <label for="user-pass" class="login-form__text-label">
+                Contraseña :
+            </label>
+            <input type="password" name="user-pass" id="user-pass" class="login-form__text-input" required>
+        </div>
+        <div class="login-form__row">
+            <input type="submit" value="Entrar" class="login-form__submit-input">
+        </div>
+    </form>
+    
+
+
+ <?php get_footer();
