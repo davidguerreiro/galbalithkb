92fa86d703c535684b5194898449730f57be0052
davidguerreiro <me@davidguerreiro.com>
2018-08-13 23:54:50 +0100

    add basic templates HTML

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Footer template
+ * 
+ * @package design-document
+ */
+
+ ?>
+
+        </main>
+        <footer class="footer">
+            <ul class="footer__primary-elements">
+                <li>&copy; 2011 - 2018 Galbalith</li>
+                <li> 
+                    Un juego desarrollado por <a href="https://github.com/davidguerreiro" target="_blank">David Guerreiro</a>
+                </li>
+            </ul>
+        </footer>
+    </body>
+ </html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Header template
+ * 
+ * @package design-document
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+       <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title>Galbalith - Design Document</title>
+       <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+       <?php wp_head(); ?>
+   </head>
+   <body>
+       <main class="main">
