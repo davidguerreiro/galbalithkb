4cbdbc5246285572bc55af60492420b88aabf143
davidguerreiro <me@davidguerreiro.com>
2018-08-15 23:37:40 +0100

    add logged in users home template

---
+++ templates/home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Home Template
+ * 
+ * Template Name: Home
+ * 
+ * This page displays the home page for logged in users
+ * 
+ * @package design-document/templates
+ */
+
+ get_header();
+
+ get_footer();
